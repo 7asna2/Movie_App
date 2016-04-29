43c81ace15730b5fb6c7f00cc07ada8413f61d86
Hasnaa Tawfik Mohammad <7asna2.tawfik11@gmail.com>
2016-04-29 23:34:53 +0200

    7th commit

---
+++ app/src/main/java/com/example/hasna2/movieapp/Models/Video.java
@@ -0,0 +1,17 @@
+package com.example.hasna2.movieapp.Models;
+
+/**
+ * Created by hasna2 on 23-Apr-16.
+ */
+public class Video {
+    // Data Base Entries
+    public final static String TABLE_NAME = "videos";
+    public final static String VIDEO_KEY="key";
+    public final static String VIDEO_MOVIE_ID="movie_id";
+    public final static String VIDEO_NAME="name";
+    public final static String VIDEO_SITE="site";
+
+    public String Name;
+    public String key;
+    public String site;
+}
